66ddaf33f666351023d88269c61bda8209597dda
Jason C <Cheeez87@gmail.com>
2021-09-20 16:41:01 -0400

    Add files via upload

---
+++ NewHomePage/app.js
@@ -0,0 +1,183 @@
+const quoteContanier = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+//Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContanier.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContanier.hidden = false;
+  loader.hidden = true;
+}
+
+//Show New Quote
+function newQuote() {
+  loading();
+  //Generate Radom Qutoe from API
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //Check if Author Field is Blank and replace with Unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //Check Quote Length for Styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Event Lsiteners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+//On Load
+getQuotes();
+
+// DOM Elements
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+
+// Options
+const showAmPm = true;
+
+// Show Time
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  // Set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12hr Format
+  hour = hour % 12 || 12;
+
+  // Output Time
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${showAmPm ? amPm : ""}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// Set Background and Greeting
+function setBgGreet() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+    greeting.textContent = "Good Morning, Jason ";
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon, Jason ";
+  } else {
+    // Evening
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/924T2Wv/night.jpg')";
+    greeting.textContent = "Good Evening, Jason ";
+    document.body.style.color = "white";
+  }
+}
+
+// Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+// Set Focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
+// Run
+showTime();
+setBgGreet();
+//getName();
+//getFocus();
